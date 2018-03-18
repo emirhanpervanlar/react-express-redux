c51a81bf29f6c4c862ca867a75288ddef8b5ec49
Emirhan <pervanlaremirhan@gmail.com>
2018-03-18 23:41:05 +0300

    Route Create

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import AuthRouter from './routes/web';
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+AuthRouter(app);
 
 app.get('/',function(req,res){
-    res.send('res API');
+    res.send("API Page");
 });
-
 
 app.listen(3300,function(){
     console.log("Server listening ...")
